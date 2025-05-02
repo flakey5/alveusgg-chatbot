7851cf5ea70bf4982d182db8a7c0ae975580e566
flakey5 <73616808+flakey5@users.noreply.github.com>
2025-05-02 15:23:31 -0700

    add permissions for uselegacy, usenew
    Signed-off-by: flakey5 <73616808+flakey5@users.noreply.github.com>

--- src/config/config.js
+++ src/config/config.js
@@ -380,7 +380,7 @@ const commandPermissionsExtra = {
     commandAdmins: ["testadminextra"],
     commandSuperUsers: ["testsuperextra", "resetcloudsource", "resetcloudsourcef", "setalveusscene", "setcloudscene", "changeserver", "setmute", "camclear"],
     commandMods: ["testmodextra", "resetsource","resetsourcef","camload", "camlist", "camsave", "camrename", "campresetremove", "customcams", "customcamsbig", "customcamstl", "customcamstr", "customcamsbl", "customcamsbr",
-        "unmutecam", "unmuteallcams", "nightcams", "nightcamsbig", "indoorcams", "addcam"],
+        "unmutecam", "unmuteallcams", "nightcams", "nightcamsbig", "indoorcams", "addcam", "uselegacy", "usenew"],
     commandOperator: ["showchat","hidechat","raidvideo","stopraidvideo","showrounds","hiderounds","checkmark","clearcheckmarks"],
     commandVips: ["getvolume", "setvolume", "resetvolume", "removecam", "swapcam", "scenecams", "mutecam", "muteallcams", "musicvolume", "musicnext", "musicprev", 
                 "mutemusic", "unmutemusic", "mutemusiclocal", "unmutemusiclocal", "resetbackpack", "resetbackpack2", "resetbackpack3", "resetpc", "resetlivecam", 
